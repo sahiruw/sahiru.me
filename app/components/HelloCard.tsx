f14d1748d73f76ada94d2bab6312a028c651019b
sahiru <sahiruwrcg@gmail.com>
2024-06-14 03:28:06 +0530

    chore: Add typewriter-effect dependency and update HelloCard component

--- app/components/HelloCard.tsx
+++ app/components/HelloCard.tsx
@@ -1,14 +1,27 @@
+'use client';
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+
 
 const HelloCard = () => {
-  return (
+    let passions = ["Computer Science Undergraduate", "Web Developer", "Software Developer", "Programmer", "Tech Enthusiast", "Learner"]
 
-      <div className='text-white absolute bottom-0 left-0 w-3/6 p-10'>
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Hello, I'm Sahiru Wijesinghe</h1>
-        <p className="text-xl md:text-2xl">A Passionate Computer Science Engineer</p>
-      </div>
+    return (
 
-  );
+        <div className='text-white absolute bottom-0 left-0 w-3/6 p-10'>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">Hello, I'm Sahiru Wijesinghe</h1>
+            <p className="text-xl md:text-2xl">A Passionate <Typewriter
+                options={{
+                    strings: passions,
+                    autoStart: true,
+                    loop: true,
+                    skipAddStyles: true
+                }}
+            /></p>
+            
+        </div>
+
+    );
 };
 
 export default HelloCard;
