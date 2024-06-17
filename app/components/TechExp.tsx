47291c403b633f0ceaf656957d434f8ff0393a47
sahiru <sahiruwrcg@gmail.com>
2024-06-18 03:57:37 +0530

    feat: Add TechExp component

---
+++ app/components/TechExp.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react'
+
+const TechExp = () => {
+    return (
+        <div className=' bg-neutral-900 my-32 w-5/6 ml-32'>
+            <h1 className="text-white text-2xl text-left my-10">Hands-on Experience</h1>
+
+            <div className='flex justify-center items-center '>
+                
+
+            </div>
+        </div>
+
+    )
+}
+
+
+
+export default TechExp
