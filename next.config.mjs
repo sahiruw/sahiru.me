e58e98afacd6fbf75952967eb61d77994c1db6ba
sahiru <sahiruwrcg@gmail.com>
2024-06-22 23:54:08 +0530

    refactor: Update next.config.mjs to export default nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/sahiru.me",
-    output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
   
-  module.exports = nextConfig;
+export default nextConfig;
