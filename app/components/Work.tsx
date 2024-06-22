2a194ed0bb6729138bc99ea02dff7b607c1457dc
sahiru <sahiruwrcg@gmail.com>
2024-06-22 23:52:43 +0530

    feat: Add Work component

---
+++ app/components/Work.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Feedback from './Feedback'
+import RecentWork from './RecentWork'
+
+const Work = () => {
+  return (
+    <>
+      <Feedback />
+      <RecentWork />
+    </>
+  )
+}
+
+export default Work
