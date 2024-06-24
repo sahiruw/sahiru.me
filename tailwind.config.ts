40db8ec686463e582c8c48a8d26186fb766f883a
sahiru <sahiruwrcg@gmail.com>
2024-06-24 06:51:27 +0530

    refactor: Remove unused zIndex values in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,21 +13,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-
-    zIndex: {
-      '0': 0,
-     '10': 10,
-     '20': 20,
-     '30': 30,
-     '40': 40,
-     '50': 50,
-     '25': 25,
-     '50': 50,
-     '75': 75,
-     '100': 100,
-      'auto': 'auto',
     }
+
   },
   plugins: [],
 };
